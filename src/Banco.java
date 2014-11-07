7254a63ef5ddbe40ef99796bdb8b2b7076151039
usrlab25 <usrlab25@usrlab25-m.(none)>
2014-11-07 21:00:14 -0200

    Conexão com o banco

---
+++ src/Banco.java
@@ -0,0 +1,45 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Banco {
+
+    public ResultSet executeSql(String sql) {
+        Statement stmt = null;
+        ResultSet rs = null;
+        try {
+
+            Connection conn = null;
+            conn =
+                    DriverManager.getConnection("jdbc:mysql://db4free.net:3306/trabalho?"
+                    + "user=steinmetz&password=steinmetz");
+
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(sql);
+
+        } catch (SQLException ex) {
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        } finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException sqlEx) {
+                }
+                rs = null;
+            }
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException sqlEx) {
+                }
+                stmt = null;
+            }
+        }
+        return rs;
+    }
+}
---
+++ src/Testes.java
@@ -0,0 +1,20 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author usrlab25
+ */
+public class Testes {
+
+    public static void main(String args[]){
+    }
+}
