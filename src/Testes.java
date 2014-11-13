796a3472c94a60ebd3a45cd3959fcd7a8e53dd32
Caio <Caio@Ultrabook>
2014-11-13 21:07:52 -0200

    Exclusao dos imports nao usados.

--- src/Testes.java
+++ src/Testes.java
@@ -1,10 +1,3 @@
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
